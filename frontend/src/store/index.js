bb3d1df3554c05ce80e31acb24953e378351650d
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-02-23 16:04:14 +0100

    Setup api methods

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,51 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import auth from "./modules/postAuth";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+   state: {
+      // token: "",
+   },
+   //
+   mutations: {
+      // submitUser(state, tokenValue) {
+      //    state.token = tokenValue;
+      //    console.log(state.token);
+      // },
+      getUser() {
+         console.log("User!");
+      },
+   },
+   actions: {
+      // async submitUser({ commit }, payload) {
+      //    const responData = await axios.post("http://localhost:5000/api/auth", payload);
+      //    commit("submitUser", responData.data.token);
+      //    console.log(responData);
+      // },
+      async getUser({ commit }, payload) {
+         axios
+            .get("http://localhost:5000/api/me", {
+               headers: {
+                  Authorization: `Bearer ${payload}`,
+               },
+            })
+            .then((res) => {
+               console.log(res);
+            })
+            .catch((err) => {
+               console.log(err);
+            });
+         commit("getUser");
+      },
+   },
+   modules: {
+      auth,
+   },
+   getters: {
+      getToken() {
+         return auth.state.token;
+      },
+   },
+});
